c2f6f4b4adccd1d60fe921673c04a3bcc3c1f470
LiuSaiSai <weiyiliusai@outlook.com>
2020-02-27 11:40:35 +0800

    完成优酷菜单，并用ViewGroup和属性视图解决其BUG

---
+++ app/src/main/java/com/example/customview/Tools.java
@@ -0,0 +1,87 @@
+package com.example.customview;
+
+import android.animation.ObjectAnimator;
+import android.provider.FontsContract;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.RotateAnimation;
+import android.widget.RelativeLayout;
+
+import androidx.interpolator.view.animation.FastOutLinearInInterpolator;
+
+/**
+ * @author LiuSaiSai
+ * @description: 优酷菜单中，控制其是否显示 圆环 的工具类
+ * 隐藏和显示 都是通过旋转实现的
+ * BUG:旋转存在一定的 bug：动画已经移动隐藏了；但是点击原来 menu 位置，依然会有都动画产生
+ * 解决方法：1. 设置隐藏后不可以点击 → view.setEnabled(false);并且 将传入的控件 从View 改成ViewGroup；
+ * 还不够；还要遍历其孩子，将每一个孩子都设置成 false
+ * 2.利用属性动画特性解决 将原来的 视图动画 替换成 属性动画ObjectAnimator
+ * @date :2020/02/27 8:44
+ */
+public class Tools {
+
+    public static void hideView(ViewGroup view) {
+        hideView(view, 0);
+    }
+
+    public static void showView(ViewGroup view) {
+        showView(view, 0);
+    }
+
+    public static void hideView(ViewGroup view, long startOffSet) {
+  /*.setDuration(500);    //设置动画的毫秒数    视图动画
+        //旋转工具类;此时传入四个参数：起始角度，结束角度，旋转中心的 X Y坐标
+        //这个 X Y坐标有瑕疵；参考系是怎样定的？控件本身的宽没有充满屏幕——效果没有错，自己的理解有瑕疵
+        RotateAnimation hideRotate = new RotateAnimation(0, 180, view.getWidth() / 2, view.getHeight());
+        hideRotate
+        hideRotate.setFillAfter(true);  //设置动画播放完成后停留在最后的 状态；
+        hideRotate.setStartOffset(startOffSet); //设置动画延迟多久后执行
+        view.startAnimation(hideRotate);
+
+        // View 和 ViewGroup 的区别，ViewGroup可以操作其孩子(控件中的子控件)；而 View 不行；导致隐藏后，孩子还可以被点击；
+        // 故将其改为ViewGroup;并且遍历其孩子，将每一个孩子都设置成 false
+        for (int count = 0; count < view.getChildCount(); count++) {
+            View childrenView = view.getChildAt(count);
+            childrenView.setEnabled(false);
+        }
+        //一、设置隐藏后不可以点击；这样不好使；没有操作其子控件
+//        view.setEnabled(false);*/
+
+        //属性动画解决 BUG  ObjectAnimator
+//        view.setRotation();
+        ObjectAnimator animator = ObjectAnimator.ofFloat(view, "rotation", 0, 180);
+        animator.setDuration(500);    //设置动画的毫秒数
+        animator.setStartDelay(startOffSet);  //设置动画播放完成后停留在最后的 状态
+        animator.start();
+
+        view.setPivotX(view.getWidth() / 2);    //设置旋转中心；错误方法 .setRotationX()
+        view.setPivotY(view.getHeight());
+    }
+
+    public static void showView(ViewGroup view, long startOffSet) {
+        /*.setDuration(500);    //设置动画的毫秒数    视图动画
+          RotateAnimation hideRotate = new RotateAnimation(180, 360, view.getWidth() / 2, view.getHeight());
+        hideRotate.setDuration(500);    //设置动画的毫秒数
+        hideRotate.setFillAfter(true);  //设置动画播放完成后停留在最后的 状态；
+        hideRotate.setStartOffset(startOffSet); //设置动画延迟多久后执行
+        view.startAnimation(hideRotate);
+
+        for (int count = 0; count < view.getChildCount(); count++) {
+            View childrenView = view.getChildAt(count);
+            childrenView.setEnabled(true);
+        }
+        //一、设置 显示 后还可以点击
+//        view.setEnabled(true);*/
+
+        //属性动画解决 BUG  ObjectAnimator
+//        view.setRotation();
+        ObjectAnimator animator = ObjectAnimator.ofFloat(view, "rotation", 180, 360);
+        animator.setDuration(500);    //设置动画的毫秒数
+        animator.setStartDelay(startOffSet);  //设置动画播放完成后停留在最后的 状态
+        animator.start();
+
+        view.setPivotX(view.getWidth() / 2);    //设置旋转中心；错误方法 .setRotationX()
+        view.setPivotY(view.getHeight());
+    }
+}
--- app/src/main/java/com/example/customview/YoukuMenu.java
+++ app/src/main/java/com/example/customview/YoukuMenu.java
@@ -1,20 +1,150 @@
 package com.example.customview;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 /**
  * @author LiuSaiSai
- * @description:优酷菜单实现.
- * 1. 关于其布局，要先添加 外面的 大的半圆，这样小圆也可以点击，
+ * @description:优酷菜单实现. 1. 关于其布局，要先添加 外面的 大的半圆，这样小圆也可以点击，
  * @date :2020/02/26 20:50
  */
-public class YoukuMenu extends AppCompatActivity {
+public class YoukuMenu extends AppCompatActivity implements View.OnClickListener {
+    private ImageView iconHome;
+    private ImageView iconMenu;
+    private RelativeLayout level1Small;
+    private RelativeLayout level2Medium;
+    private RelativeLayout level3Large;
+
+    /**
+     * 设置是否显示第三个圆环；
+     * true  为显示：
+     * false 为隐藏
+     */
+    private boolean isShowLevel3 = true;
+
+    /**
+     * 设置是否显示第二个圆环；
+     * true  为显示：
+     * false 为隐藏
+     */
+    private boolean isShowLevel2 = true;
+
+    /**
+     * 设置是否显示第一个圆环；
+     * true  为显示：
+     * false 为隐藏
+     */
+    private boolean isShowLevel1 = true;
+    private Button youkuMenuButton;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_youku_menu);
+
+        iconHome = findViewById(R.id.home_youku_menu);
+        iconMenu = findViewById(R.id.menu_youku_menu);
+        level1Small = findViewById(R.id.level1_small);
+        level2Medium = findViewById(R.id.level2_medium);
+        level3Large = findViewById(R.id.level3_large);
+        youkuMenuButton = findViewById(R.id.title_edit_button);
+        youkuMenuButton.setText("菜单");
+
+        // 设置单击事件
+        iconHome.setOnClickListener(this);
+        iconMenu.setOnClickListener(this);
+        youkuMenuButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.home_youku_menu:
+                //如果大菜单和中菜单都是显示的，让其隐藏
+                //如果大菜单和中菜单都是隐藏的，二级菜单显示
+                if (isShowLevel3) {
+                    isShowLevel2 = false;
+                    isShowLevel3 = false;
+                    Tools.hideView(level2Medium, 200);
+                    Tools.hideView(level3Large);
+                } else if (isShowLevel2) {
+                    isShowLevel2 = false;
+                    Tools.hideView(level2Medium);
+                } else {
+                    isShowLevel2 = true;
+                    Tools.showView(level2Medium);
+                }
+
+                break;
+            case R.id.menu_youku_menu:
+                //如果显示，让其隐藏；如果隐藏让其显示
+                if (isShowLevel3) {
+                    isShowLevel3 = false;
+                    Tools.hideView(level3Large);
+                } else {
+                    isShowLevel3 = true;
+                    Tools.showView(level3Large);
+                }
+                break;
+            case R.id.title_edit_button:
+                //设置右上角的点击事件；如果菜单存在；则让其隐藏；
+                if (isShowLevel1 || isShowLevel2 || isShowLevel3) {
+                    if (isShowLevel3) {
+                        isShowLevel3 = false;
+                        Tools.hideView(level3Large);
+                    }
+                    if (isShowLevel2) {
+                        isShowLevel2 = false;
+                        Tools.hideView(level2Medium, 200);
+                    }
+                    if (isShowLevel1) {
+                        isShowLevel1 = false;
+                        Tools.hideView(level1Small, 400);
+                    }
+                }
+                //如果隐藏，就让其显示
+                else {
+                    isShowLevel1 = true;
+                    isShowLevel2 = true;
+
+                    Tools.showView(level1Small);
+                    Tools.showView(level2Medium, 200);
+                }
+            default:
+                break;
+        }
+    }
+
+    /**
+     * 监听物理键；但是不好使，menu菜单启动多任务；查网上说需要设置广播 × × × × 没有尝试继续做
+     */
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_MENU) {
+            //如果 一、二、三 级菜单都显示就都让其隐藏；
+            if (isShowLevel3) {
+                isShowLevel1 = false;
+                isShowLevel2 = false;
+                isShowLevel3 = false;
+
+                Tools.hideView(level1Small, 400);
+                Tools.hideView(level2Medium, 200);
+                Tools.hideView(level3Large);
+            }
+            //如果 一、二、三 级菜单都隐藏就显示 一、二 级菜单
+            if (!(isShowLevel1 && isShowLevel2 && isShowLevel3)) {
+                Tools.showView(level1Small);
+                Tools.showView(level2Medium, 200);
+            }
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
