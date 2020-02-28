66983130de123ed8521ce3d6d5bd7d821e3ce687
LiuSaiSai <weiyiliusai@outlook.com>
2020-02-28 12:21:33 +0800

    修复点消失BUG(点小)；设置了点击事件。结束了广告条

--- app/src/main/java/com/example/customview/AdvanceItem.java
+++ app/src/main/java/com/example/customview/AdvanceItem.java
@@ -9,9 +9,8 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
@@ -24,13 +23,14 @@ import java.util.List;
  * @description:实现广告条效果；可以滑动；下边有点数，可以循环，每一页下面有文字备注 设置了无限循环，怎么突然小红点消失了?
  * 实现自动滑动？1. for + sleep ? 2. handler
  * 按住就不再自动滑动
+ *  BUG： 小红点消失了？！ 实际上是 params 设置的太小了，看不到！气死了
  * @date :2020/02/27 11:50
  */
 public class AdvanceItem extends AppCompatActivity {
 
-    private LinearLayout mPointAdvanceItem;
-    private TextView mTextAdvanceItem;
     private ViewPager mViewPager;
+    private TextView mTextAdvanceItem;
+    private LinearLayout mPointAdvanceItem;
 
     /**
      * ViewPager 的使用，类似于 ListView;
@@ -49,14 +49,17 @@ public class AdvanceItem extends AppCompatActivity {
             R.drawable.advance_woods_path,};
     //  记录 上一个点的位置
     private int prePosition = 0;
-    //广告标题集合
+    // 是否 已经 滑动
+    private boolean isDragging = false;    //广告标题集合
+
     private final String[] imageDescriptions = {
             "3D环绕", "广袤森林", "一路向前", "万园之园", "", "俯瞰马路"
     };
 
+
     private Handler mHandler = new Handler() {
         @Override
-        public void handleMessage(@NonNull Message msg) {
+        public void handleMessage(Message msg) {
             super.handleMessage(msg);
             int item = mViewPager.getCurrentItem() + 1;
             mViewPager.setCurrentItem(item);
@@ -66,7 +69,7 @@ public class AdvanceItem extends AppCompatActivity {
     };
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_advance_item);
 
@@ -74,37 +77,43 @@ public class AdvanceItem extends AppCompatActivity {
         mTextAdvanceItem = findViewById(R.id.text_advance_item);
         mPointAdvanceItem = findViewById(R.id.point_advance_item);
 
+        // ViewPager 的使用，类似于 ListView;
+        // 1. 布局中定义；
+        // 2. 代码中实例化；
+        // 3. 准备数据；
         mImageViewList = new ArrayList<>();
         for (int i = 0; i < imageIds.length; i++) {
 
             ImageView imageView = new ImageView(this);
             //这里使用的是 setBackgroundResource；不是 src 避免图片不能充满；也不是setBackground
             imageView.setBackgroundResource(imageIds[i]);
-            mImageViewList.add(imageView);  //添加到自己的 数据集合中
+
+            //添加到自己的 数据集合中
+            mImageViewList.add(imageView);
 
             // 添加点
             ImageView point = new ImageView(this);
-            point.setBackgroundResource(R.drawable.point_selector);
+            point.setBackgroundResource(R.drawable.point_selector);//丢了关键一步！
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(28, 28);
+
             //  params 用于设置 点的 间隔
-            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(8, 8);
             if (i == 0) {
                 point.setEnabled(true);     //显示红色
             } else {
                 point.setEnabled(false);    //显示灰色
-                params.leftMargin = 8;      //不是 第0 个点，就距离左边 8 个像素。
+                params.leftMargin = 18;      //不是 第0 个点，就距离左边 8 个像素。
             }
-
             point.setLayoutParams(params);
             mPointAdvanceItem.addView(point);
         }
-        //设置适配器
+        //4. 设置适配器
         mViewPager.setAdapter(new MyPagerAdapter());
         //设置监听 点的位置的改变
         mViewPager.addOnPageChangeListener(new MyOnPageChangeListener());
 
         // 存在的 BUG ；可以无限向右滑动，但初始时，不能左滑；解决：将初始位置设置在中间
         //如果你想初始位置在首页，那么就让其能整除 页面总数；
-        mViewPager.setCurrentItem(200);
+        mViewPager.setCurrentItem(50);
 
         mTextAdvanceItem.setText(imageDescriptions[prePosition]);
 
@@ -152,10 +161,14 @@ public class AdvanceItem extends AppCompatActivity {
         @Override
         public void onPageScrollStateChanged(int state) {
             if (state == ViewPager.SCROLL_STATE_DRAGGING) {   //拖拽
-
+                isDragging = true;
+                mHandler.removeCallbacksAndMessages(null);
             } else if (state == ViewPager.SCROLL_STATE_SETTLING) {
 
-            } else if (state == ViewPager.SCROLL_STATE_IDLE) { //空闲
+            } else if (state == ViewPager.SCROLL_STATE_IDLE && isDragging) { //空闲
+                isDragging = false;
+                mHandler.removeCallbacksAndMessages(null);
+                mHandler.sendEmptyMessageDelayed(0, 4000);
             }
         }
 
@@ -166,19 +179,7 @@ public class AdvanceItem extends AppCompatActivity {
         @Override
         public int getCount() {
 //            return mImageViewList.size(); //本方法无法实现 无限循环
-            return 400;
-        }
-
-        /**
-         * 比较 view 和 object 是不是一个实例
-         *
-         * @param view
-         * @param object
-         * @return
-         */
-        @Override
-        public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
-            return view == object;
+            return 100;
         }
 
         /**
@@ -188,11 +189,11 @@ public class AdvanceItem extends AppCompatActivity {
          * @param position  当前实例化页面的位置
          * @return
          */
-        @NonNull
         @Override
-        public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        public Object instantiateItem(ViewGroup container, int position) {
             int realPosition = position % mImageViewList.size();    //实现无限循环时，需要对其取模；
-            ImageView imageView = mImageViewList.get(realPosition);
+
+            final ImageView imageView = mImageViewList.get(realPosition);
             container.addView(imageView);
             imageView.setOnTouchListener(new View.OnTouchListener() {
                 @Override
@@ -204,8 +205,8 @@ public class AdvanceItem extends AppCompatActivity {
                         case MotionEvent.ACTION_MOVE:   //手指滑动
                             break;
                         case MotionEvent.ACTION_CANCEL:   //手指滑动
-                            mHandler.removeCallbacksAndMessages(null);
-                            mHandler.sendEmptyMessageDelayed(0, 3000);
+//                            mHandler.removeCallbacksAndMessages(null);
+//                            mHandler.sendEmptyMessageDelayed(0, 3000);
                             break;
                         case MotionEvent.ACTION_UP:     //手指离开
                             mHandler.removeCallbacksAndMessages(null);
@@ -214,11 +215,39 @@ public class AdvanceItem extends AppCompatActivity {
                         default:
                             break;
                     }
-                    return true;
+                    return false;
+                }
+            });
+            /**
+             * 设置图片的点击事件；
+             * 1. 设置 .setTag() 是为了指导此处是哪个位置;
+             * 2. 将触摸事件返回 false； 否则点击事件没有用;
+             * 3. position 取模，不然就角标越界。
+             */
+            imageView.setTag(position);
+            imageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position = (int) v.getTag() % mImageViewList.size();
+                    String imageDescriptionText = imageDescriptions[position];
+                    Toast.makeText(AdvanceItem.this, imageDescriptionText, Toast.LENGTH_SHORT).show();
                 }
             });
             return imageView;
         }
+
+        /**
+         * 比较 view 和 object 是不是一个实例
+         *
+         * @param view
+         * @param object
+         * @return
+         */
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
+
 
         /**
          * 释放资源
@@ -228,7 +257,7 @@ public class AdvanceItem extends AppCompatActivity {
          * @param object    要释放的界面
          */
         @Override
-        public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        public void destroyItem(ViewGroup container, int position, Object object) {
             container.removeView((View) object);
         }
     }
