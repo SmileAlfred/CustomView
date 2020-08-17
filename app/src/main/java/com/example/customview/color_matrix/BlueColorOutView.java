93945b14af11bded5e6dab3aa73d62b045ea9bda
LiuSaiSai <weiyiliusai@outlook.com>
2020-08-17 21:47:08 +0800

    添加彩图的蓝色通道输出

---
+++ app/src/main/java/com/example/customview/color_matrix/BlueColorOutView.java
@@ -0,0 +1,9 @@
+package com.example.customview.color_matrix;
+
+/**
+ * @author: LiuSaiSai
+ * @date: 2020/08/17 21:14
+ * @description:
+ */
+class BlueColorOutView extends View {
+}
---
+++ app/src/main/java/com/example/customview/color_matrix/ColorMatrixActivity.java
@@ -0,0 +1,9 @@
+package com.example.customview.color_matrix;
+
+/**
+ * @author: LiuSaiSai
+ * @date: 2020/08/17 20:30
+ * @description:
+ */
+class ColorMatrixActivity extends AppcompatActivity {
+}
